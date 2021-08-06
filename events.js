6c674835eb7d7112cb7961a77e081d5f1835367b
PhilComcave <p.pick@kladusch.de>
2021-08-06 12:02:58 +0200

    button text switch gebaut

--- events.js
+++ events.js
@@ -1,5 +1,6 @@
 
 /********  Variablen **********/
+const btn = document.getElementById("trigBtn");
 
 /*********** Änderung in View-Schicht ************/
 
@@ -11,6 +12,14 @@ function switchClassName(modeStr)
     document.body.className = modeStr;
     document.body.children[0].className = modeStr;
     document.body.children[1].className = modeStr;
+}
+
+// Modul: Button-Funktion | Test:
+//switchBtnTxt("night");
+//switchBtnTxt("day");
+function switchBtnTxt(modeStr)
+{
+    btn.innerHTML = modeStr;
 }
 
 /* Tools */
