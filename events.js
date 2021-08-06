7323fa6aab59b69cdf0fd3c22029d7ae89f02495
PhilComcave <p.pick@kladusch.de>
2021-08-06 11:51:46 +0200

    class gesetzt und Funktion zum switchen gesetzt

--- events.js
+++ events.js
@@ -1,6 +1,17 @@
 
 /********  Variablen **********/
 
+/*********** Änderung in View-Schicht ************/
+
+// Modul: Umschaltung der Klassennamen | Test:
+//switchClassName("night");
+//switchClassName("day");
+function switchClassName(modeStr)
+{
+    document.body.className = modeStr;
+    document.body.children[0].className = modeStr;
+    document.body.children[1].className = modeStr;
+}
 
 /* Tools */
 function ausgabe(outputStr) {
